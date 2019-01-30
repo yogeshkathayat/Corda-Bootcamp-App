50734e56cac7cd3b6cb121e277cf32188a4dca01
yogesh <kathayat.yogesh@gmail.com>
2019-01-30 18:40:27 +0400

    added IOU flow for issuing an IOU into the ledger

--- cordapp/src/test/java/com/template/FlowTests.java
+++ cordapp/src/test/java/com/template/FlowTests.java
@@ -13,8 +13,8 @@ public class FlowTests {
     private final StartedMockNode b = network.createNode();
 
     public FlowTests() {
-        a.registerInitiatedFlow(Responder.class);
-        b.registerInitiatedFlow(Responder.class);
+        a.registerInitiatedFlow(IOUFlow.class);
+        b.registerInitiatedFlow(IOUFlow.class);
     }
 
     @Before
